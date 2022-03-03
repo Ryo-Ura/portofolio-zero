a0f5199a1d5dc3433f26093f0adbee8c4c951300
Ryo-Ura <urawaryosuke@gmail.com>
2022-03-03 12:55:44 -0800

    added temporal pictures on the first 2 fields, which is expected to be replaced

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -2,6 +2,25 @@ import "./portofolio.scss"
 
 export default function Portofolio() {
   return (
-    <div className='portofolio' id="portofolio">Portofolio</div>
+    <div className='portofolio' id="portofolio">
+      <h1>Portofolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+
+      </ul>
+      <div className="container">
+        <div className="item">
+          <a href="#works">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg" alt="Відьомські пороги на світанку.jpg"  />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg" alt="Відьомські пороги на світанку.jpg"  />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg" alt="Відьомські пороги на світанку.jpg"  />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg" alt="Відьомські пороги на світанку.jpg"  />
+          </a>
+        </div>
+      </div>
+
+    </div>
   )
 }
