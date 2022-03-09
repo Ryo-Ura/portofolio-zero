6efc6f9599f1d41d915168931f4494c7518234ff
Ryo-Ura <urawaryosuke@gmail.com>
2022-03-08 22:46:46 -0800

    basic component for popup window in portofolio section

---
+++ src/components/portofolio/popup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function popup() {
+    return (
+        <div className='background'>
+            <div className='container'>
+                <button>X</button>
+                <div className='title'>
+                    title
+                </div>
+                <div className='body'>
+                    <p>
+                        body 
+                    </p>
+                </div>
+                <div className='footer'></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default popup
