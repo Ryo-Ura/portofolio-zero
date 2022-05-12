c58171252205e676cfe69913240838b881f182fb
Ryo-Ura <urawaryosuke@gmail.com>
2022-05-12 04:49:16 -0700

    displays new image on the portofolia section

--- src/data.js
+++ src/data.js
@@ -22,14 +22,14 @@ export const featuredPortofolio = [
     },
     {
         id: 4,
-        title: "currently working",
-        img: imageNotReady,
-        alt : defaltAlt,
+        title: "To-Do list",
+        img: "assets/Todo.png",
+        alt : "https://github.com/Ryo-Ura/Wantedly-Todos",
     },
     {
         id: 5,
         title: "currently working",
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg",
+        img: imageNotReady,
         alt : defaltAlt,
     },
     {
@@ -56,6 +56,12 @@ export const webPortofolio = [
     },
     {
         id: 2,
+        title: "To-Do list",
+        img: "assets/Todo.png",
+        alt : "https://github.com/Ryo-Ura/Wantedly-Todos",
+    },
+    {
+        id: 3,
         title: "currently working",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg/1200px-%D0%92%D1%96%D0%B4%D1%8C%D0%BE%D0%BC%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D0%BE%D1%80%D0%BE%D0%B3%D0%B8_%D0%BD%D0%B0_%D1%81%D0%B2%D1%96%D1%82%D0%B0%D0%BD%D0%BA%D1%83.jpg",
         alt: defaltAlt,
